d44f29adbca59b5d34927914fbd289d38823db24
jose-tejaxun <danieltejaxun123@gmail.com>
2025-05-11 19:50:53 -0600

    alojamiento de direcciones

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, user, admin
 
-app = FastAPI(title="MindSpace API")
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # para pruebas locales
+    "https://mind-space-frontend.vercel.app"  # tu frontend en Vercel
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user")
